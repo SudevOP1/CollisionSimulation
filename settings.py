7c1ddcadf1f771d50e0fcf43c8621ac22627a55f
SudevOP1 <sudevdahitule06@gmail.com>
2025-06-22 19:33:32 +0530

    stopped tracking logs.csv

--- settings.py
+++ settings.py
@@ -27,14 +27,14 @@ FONT_XPAD = 20
 FONT_YPAD = 5
 
 # for logs
-TIME_PLAYBACK_SPEED = 1
-END_SIMULATION_AT = -1 # auto stop after n seconds (put -1 to disable)
+TIME_PLAYBACK_SPEED = 1200
+END_SIMULATION_AT = 1200 # auto stop after n seconds (put -1 to disable)
 LOGS_FILENAME = "logs"
 SAVE_KE_AND_P = True
 ROUND_DIGITS = 4
 
 # sound
-PLAY_COLLISION_SOUND = True
+PLAY_COLLISION_SOUND = False
 COLLISION_SOUND_FILENAME = "3b1b_clack_sound.mp3" # https://www.3blue1brown.com/lessons/clacks-via-light
 
 # others
